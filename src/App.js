6470e25ab8e80bd59e76119f069b0cff26df6b0c
AKSHAY <akshayjadhav461999@gmail.com>
2020-12-26 13:56:22 +0530

    getting weather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, { useState } from "react";
 import "./App.css";
+import { fetchWeatherData } from "./api/fetchWeather";
+import Search from "./components/Search/Search";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState({});
+
+  const getWeather = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeatherData(city);
+      setWeatherData(data);
+      setCity("");
+    }
+  };
+
   return (
-    <div className="app">
-      <h1 className="text-xl font-medium text-green-400">Hello World</h1>
+    <div className="container mx-auto  w-6/12">
+      {/* Search */}
+      <Search city={city} setCity={setCity} getWeather={getWeather} />
     </div>
   );
 }
